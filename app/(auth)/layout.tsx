ffb9d9f5e63f6fadc6e0ab3c1d97432d91bca1c5
Fadhil <fadhilsaheer@gmail.com>
2024-01-14 21:56:57 +0530

    routes

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const AuthLayout: React.FC = () => {
+  return <div>layout</div>;
+};
+
+export default AuthLayout;
