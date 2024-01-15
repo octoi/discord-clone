ade0905b6168eafb6b49fdd770f10b4cf4efa7de
Fadhil <fadhilsaheer@gmail.com>
2024-01-15 21:05:44 +0530

    feat: modal

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from 'next/navigation';
 import { initialProfile } from '@/lib/initial-profile';
 import { db } from '@/lib/db';
+import { InitialModal } from '@/components/modals/initial-modal';
 
 export const SetupPage = async () => {
   const profile = await initialProfile();
@@ -19,5 +20,7 @@ export const SetupPage = async () => {
     return redirect(`/servers/${server.id}`);
   }
 
-  return <div>Create a Server</div>;
+  return <InitialModal />;
 };
+
+export default SetupPage;
